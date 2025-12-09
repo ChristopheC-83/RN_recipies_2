17229440aa4c0f58263f6805d567de3b63fdc7d2
kikisan <kiketdule@gmail.com>
2025-12-09 21:36:52 +0100

    installation package tabs bottom

--- App.js
+++ App.js
@@ -1,50 +1,11 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { View, Text, Image, Pressable } from "react-native";
-import HomeScreen from "./src/screens/Home/Home";
-import TestScreen from "./src/screens/Test/Test";
-import RecipiesList from "./src/screens/RecipiesList/RecipiesList";
-import RecipieDetails from "./src/screens/RecipeDetails/RecipeDetails";
-import TasksScreen from "./src/screens/TasksScreen/TasksScreen";
 
-const Stack = createNativeStackNavigator();
+import AppNavigation from "./src/navigation";
 
 export default function App() {
   return (
     // <SafeAreaProvider>
     //   <SafeAreaView style={{ flex: 1, backgroundColor: "#ddd" }}>
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Tasks List"
-        //  on centre le titre
-        screenOptions={{
-          headerTitleAlign: "center",
-        }}
-      >
-        {/* initialRouteName permet de choisir l'ecran d'accueil sinon 1er stack pack.screen par defaut */}
-        <Stack.Screen name="Recipes List" component={RecipiesList} />
-        <Stack.Screen name="Tasks List" component={TasksScreen} />
-        <Stack.Screen
-          name="Recipes Details"
-          component={RecipieDetails}
-          options={({ navigation }) => ({
-            headerLeft: () => (
-              <Pressable
-                onPress={() => navigation.goBack()} // ✅ ici le vrai retour
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginEnd: 10,
-                }}
-              >
-                <Text>{"← "}Liste</Text>
-              </Pressable>
-            ),
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppNavigation  />
     //   </SafeAreaView>
     // </SafeAreaProvider>
   );
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,46 @@
-import { View , Text} from 'react-native';
-import {s} from './index.style';
 
-export default function index() {
-    return (
-      <View style={s}>
-        <Text>index</Text>
-      </View>
-    );
-};
+import { s } from "./index.style";
+import { View, Text, Image, Pressable } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import RecipiesList from "../screens/RecipiesList/RecipiesList";
+import TasksScreen from "../screens/TasksScreen/TasksScreen";
+import RecipieDetails from "../screens/RecipeDetails/RecipeDetails";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Recipes List"
+        //  on centre le titre
+        screenOptions={{
+          headerTitleAlign: "center",
+        }}
+      >
+        {/* initialRouteName permet de choisir l'ecran d'accueil sinon 1er stack pack.screen par defaut */}
+        <Stack.Screen name="Recipes List" component={RecipiesList} />
+        <Stack.Screen name="Tasks List" component={TasksScreen} />
+        <Stack.Screen
+          name="Recipes Details"
+          component={RecipieDetails}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <Pressable
+                onPress={() => navigation.goBack()} // ✅ ici le vrai retour
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginEnd: 10,
+                }}
+              >
+                <Text>{"← "}Liste</Text>
+              </Pressable>
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
